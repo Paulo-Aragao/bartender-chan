592de7fadc85954b14e053cac1e5c3d9fbf8166b
paulo aragao <paulotorres12@gmail.com>
2025-03-21 17:42:28 -0300

    hierarchy helper and assets added

---
+++ Assets/_Project/Scripts/Utils/HierarchySeparator.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace PauloAragao.Utils
+{
+    [AddComponentMenu("Hierarchy Tools/Hierarchy Separator")]
+    public class HierarchySeparator : MonoBehaviour
+    {
+        public Color separatorColor = Color.gray;
+    }
+}
+
+
